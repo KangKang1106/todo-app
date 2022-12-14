8e0c361081058a0746fa38bdc60dd71a3dc95bdd
kangkang1106 <panda3762@naver.com>
2022-12-14 14:52:35 +0900

    feat: input 유효성 검사

--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -7,10 +7,24 @@ const AddTodo = ({ addItem }) => {
     title: "",
   });
 
+  const titleRef = useRef();
+
+  const titleFocus = () => {
+    titleRef.current.focus();
+  };
+
   const onButtonClick = () => {
     // props로 받아온 addItem 함수 실행
-    addItem(todoItem); // {title: input 입력값}
-    setTodoItem({ title: "" });
+    if (
+      todoItem.title === "" ||
+      todoItem.title.replace(/(\s*)/g, "", "") === ""
+    ) {
+      alert("할 일을 입력해주세요");
+      titleRef.current.focus();
+    } else {
+      addItem(todoItem); // {title: input 입력값}
+      setTodoItem({ title: "" });
+    }
   };
 
   const onKeyPress = (e) => {
@@ -27,7 +41,9 @@ const AddTodo = ({ addItem }) => {
         value={todoItem.title}
         onChange={(e) => setTodoItem({ title: e.target.value })}
         onKeyPress={onKeyPress}
+        ref={titleRef}
         placeholder="Add your new Todo"
+        autoFocus
       />
       <button className="addBtn" onClick={onButtonClick}>
         <FontAwesomeIcon icon={faPlus} />
